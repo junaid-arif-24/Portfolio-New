9961e7f1cf4b3ceca4a0bcbeb5277d8df7f5d1a6
junaid-arif-24 <mohdjunaid7078@gmail.com>
2023-09-11 22:02:03 +0530

    Updated Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   github: 'https://github.com/junaid-arif-24',
   resume:
-    'https://drive.google.com/file/d/1Q6J7a0MkvwntMb84VhQXBUV76m94nfzP/view?usp=sharing',
+    'https://drive.google.com/file/d/1r0o4pd1uZcEYIElKkOtQDsggfa9IMUPj/view?usp=sharing',
   linkedin: 'https://www.linkedin.com/in/mohd-junaid-24sep2000',
   twitter: 'https://twitter.com/JunaidArif24',
   insta: 'https://www.instagram.com/junaid_arif24/',
